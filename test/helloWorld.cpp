99ecd07dbabcfa8f572bdd5c14becaee5a618589
Yuze Ye in Windows11 <yeyuze000@gmail.com>
2026-03-09 00:44:38 +0800

    测试

--- test/helloWorld.cpp
+++ test/helloWorld.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "Please enter a:";
     cin >> a;
     cout << "a = " << a << endl;
-    if (a > 1) {
+    if (a >= 1) {
         cout << "fuck u" << endl;
     } else {
         cout << "suck dk" << endl;
