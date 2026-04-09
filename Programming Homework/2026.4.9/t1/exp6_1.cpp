458164949498495b7ac97859d7ce5469d8f294fd
Yuze Ye in Windows11 <yeyuze000@gmail.com>
2026-04-09 23:12:51 +0800

    完成2026.4.9 t1的一部分（问答题还没写）

---
+++ Programming Homework/2026.4.9/t1/exp6_1.cpp
@@ -0,0 +1,190 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 0
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+// 学生信息类
+class Student
+{
+public:
+    int Id;        // 学号
+    char Name[20]; // 姓名
+
+    Student() {};
+
+    Student(int id, const char *name)
+    {
+        Id = id;
+        strcpy(Name, name);
+    }
+};
+
+// 课程类
+class Course
+{
+private:
+    char courseName[1024]; // 课程的名称
+    Student *stuList;      // 选课学生名单，名单长度即预设选课人数容量
+    int courseCapacity;    // 预设的课程选课人数容量
+    int numberOfStudents;  // 已选课学生人数
+
+public:
+    Course(const char *, int);           // 构造函数，设置课程的名称、选课人数容量
+    Course(const Course &);              // 复制构造函数
+    ~Course();                           // 析构函数
+    char *GetCourseName() const;         // 获得课程名称
+    void SetCourseName(const char *);    // 修改课程名称
+    bool AddStudent(const Student &stu); // 增加一位选课学生
+    bool DropStudentbyId(int id);        // 按学号删除一位退选学生
+    int getNumberOfStudents() const;     // 获得已选课学生人数
+    void PrintStudents();                // 输出已选课学生名单
+};
+
+Course::Course(const char *newCourse, int capacity)
+{
+    // 填写1，完成构造函数（8分）
+    //  capacity表示该课程选课人数容量
+    //  请根据课程选课人数容量对stuList进行必要的内存分配
+    //   newCourse指向的字符串表示课程名称
+    //  需要将 newCourse 指针所指向的字符串，全部拷贝到couseName 指向的字符数组中
+    //  可以使用循环结构逐个字符进行拷贝，特别注意字符串以 '\0' 结尾
+    //  也可以调用 strcpy函数完成此项功能，strcpy函数原型：char * strcpy(char* _des, const char* scr);
+    //  默认已选课人数为0
+    numberOfStudents = 0;
+    courseCapacity = capacity;
+    stuList = new Student[capacity];
+    strcpy(courseName, newCourse);
+    // 评分标准：4个成员变量的初始化各2分
+}
+
+Course::Course(const Course &course) // 复制构造函数
+{
+    // 填写2，完成复制构造函数（12分）
+    // 即要将 course 中所有的成员复制到本对象中
+    // 注意对 courseName进行必要的内存分配
+    numberOfStudents = course.numberOfStudents;
+    courseCapacity = course.courseCapacity;
+    stuList = new Student[courseCapacity];
+    strcpy(courseName, course.courseName);
+    for (int i = 0; i < numberOfStudents; i++) {
+        stuList[i] = course.stuList[i];
+    }
+}
+
+Course::~Course() // 析构函数
+{
+    // 填写3，完成析构函数（3分）
+    // 参照构造函数，完成必要的内存释放
+    if (stuList != nullptr) {
+        delete[] stuList;
+    }
+    stuList = nullptr;
+}
+
+char *Course::GetCourseName() const
+{
+    return (char *)courseName;
+}
+
+void Course::SetCourseName(const char *name)
+{
+    strcpy(courseName, name);
+}
+
+bool Course::AddStudent(const Student &stu)
+{
+    // 填写4，完成添加一位选课同学的功能          （10分）
+    // 注意判断是否超过选课容量，分情况处理：
+    // 如果选课人数未达上限，可以将该学生添加到stuList中，选课人数加1，函数返回true；
+    // 否则，表示该学生选课失败，函数返回false即可。
+    if (numberOfStudents == courseCapacity) {
+        return false;
+    }
+    numberOfStudents++;
+    stuList[numberOfStudents - 1] = stu;
+    return true;
+}
+
+bool Course::DropStudentbyId(int id)
+{
+    // 填写5，完成删除一位退选学生的功能          （10分）
+    // 请遍历选课学生名单stuList，将当前学生的学号与退选学生的学号进行比较：
+    // 如匹配，则停止遍历，并从查找到的位置开始，把后面的学生从后往前依次顺移一位，同时选课人数减1，函数返回true；
+    // 如遍历整个名单没有找到匹配项，表示该学生并未选这门课，函数直接返回false即可。
+    for (int i = 0; i < numberOfStudents; i++) {
+        if (stuList[i].Id == id) {
+            for (int j = i; j < numberOfStudents - 1; j++) {
+                stuList[j] = stuList[j + 1];
+            }
+            numberOfStudents--;
+            return true;
+        }
+    }
+    return false;
+}
+
+int Course::getNumberOfStudents() const
+{
+    return numberOfStudents;
+}
+
+void Course::PrintStudents()
+{
+    if (numberOfStudents > 0) {
+        cout << "选课学生名单：" << endl;
+        for (int i = 0; i < numberOfStudents; i++) {
+            cout << left << setw(6) << stuList[i].Id << stuList[i].Name << endl;
+        }
+    } else {
+        cout << "暂无人选课，请核实。" << endl;
+    }
+}
+
+int main()
+{
+    Course course1("C++ Proramming", 2);
+    cout << "课程名称：";
+    cout << course1.GetCourseName() << endl;
+
+    cout << "现有2名学生选修该课程..." << endl;
+    course1.AddStudent(Student(101, "John"));
+    course1.AddStudent(Student(211, "Jack"));
+    cout << "当前选课人数：";
+    cout << course1.getNumberOfStudents() << endl;
+    course1.PrintStudents();
+    cout << endl;
+
+    cout << "Mike（Id:102）请求选课" << endl;
+    bool result = course1.AddStudent(Student(102, "Mike"));
+    if (result)
+        cout << "选课成功" << endl;
+    else
+        cout << "该课程选课人数已满，选课失败！" << endl;
+    course1.PrintStudents();
+    cout << endl;
+
+    cout << "新增一门课程（默认选课名单与C++相同）：" << endl;
+    Course course2(course1);
+    course2.SetCourseName("Java Lanuague");
+    cout << "课程名称：";
+    cout << course2.GetCourseName() << endl;
+    cout << "当前选课人数：";
+    cout << course1.getNumberOfStudents() << endl;
+    course2.PrintStudents();
+    cout << endl;
+
+    cout << "John（Id:101）请求退课" << endl;
+    if (course2.DropStudentbyId(101))
+        cout << "退课成功" << endl;
+    course2.PrintStudents();
+    cout << endl;
+
+    return 0;
+}
+
+// 问答（7分，请在注解中直接作答）
+//  1、在Course类中，复制构造函数是否有必要重载？ 请总结一下，一般什么情况下必须要重载复制构造函数？（3分）
+//
+//  2、在本题中，Student类的2个数据成员均设为公有成员变量，从数据安全的角度并不推荐这么设计，
+//       如果把它们设为私有成员变量，会影响Course类中哪几个成员函数的正常使用，你有什么解决办法？（4分）
---
+++ Programming Homework/2026.4.9/t2/exp6_2.cpp
@@ -0,0 +1,120 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS 0
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Time
+{
+private:
+    int day;            // 表示天数
+    int hour;           // 表示小时数，值不超过24
+    int minute;        // 表示分钟数，值不超过60
+    int second;       // 表示秒数，值不超过60
+
+    // 仅返回"时:分:秒" 表示的总秒数（不包含天数部分）
+    // 即对Time对象中，不超过24小时的那部分时间换算成秒数
+    int toSeconds() const
+    {
+        return (hour * 60 + minute) * 60 + second;
+    }
+
+public:
+    // 使用表示时间的总秒数来构造Time对象，总秒数是个非负整数
+    // 即将总秒数totalseconds换算成天数、小时数、分钟数和秒数。
+    Time(int totalseconds = 0)
+    {
+        second = totalseconds % 60;
+        int mm = totalseconds / 60;
+        minute = mm % 60;
+        int hh = mm / 60;
+        hour = hh % 24;
+        day = hh / 24;
+    }
+
+    int GetDay() const { return day; }
+    int GetHour() const { return hour; }
+    int GetMinute() const { return minute; }
+    int GetSecond() const { return second; }
+
+    // 实现时间对象到字符串格式的转换，用于输出到屏幕
+    string ToString()
+    {
+        char  strToFill[512];
+        int len;
+        if (day != 0)
+            len = sprintf(strToFill, "%d天 %02d:%02d:%02d\0", day, hour, minute, second);
+        else
+            len = sprintf(strToFill, "%02d:%02d:%02d\0", hour, minute, second);
+        return string(strToFill);
+    }
+
+    // 填写1：请在此补充必要的成员函数声明（包括构造函数等）（5分）
+
+};
+
+//填写2： 根据主函数和部分成员函数中涉及到的Time类对象的多种创建场景，
+//              在【类外】补充必要的构造函数（不限制构造函数的数量）（15分）
+
+
+// Time类成员函数
+// 计算当前时刻t1 到 t2 时刻流逝的时间（t1即到t2的时间间隔）
+// 调用时要求满足： t1 < t2
+Time Time::GetTimeInterval(Time t2)
+{
+    int timedelta = t2.toSeconds() - toSeconds();   //  获取时间部分的总秒数差
+    int dd = t2.day - day;   // 获取天数差
+
+    // 如果总秒数差小于0，则需要从t2的天数中借1天（24小时）
+    // 再把这1天的时间转换成86400秒，叠加到当前总秒数差中，以保证总秒数差值是个正数
+    if (timedelta < 0)
+    {
+        dd--;                          // t的天数减1
+        timedelta += 86400; // 时间差加上1天所表示的秒数
+    }
+
+    // 注意：这里使用总秒数构造了一个Time对象tt
+    // 该对象其实仅表示t1到t2所流逝的时间的时，分，秒部分，天数部分的计算结果存储在变量dd中。
+    Time tt(timedelta);
+
+    // 函数返回一个Time对象，该对象通过时、分、秒和天数这4个参数进行构造（注意参数次序）。
+    // 其中，分和秒的输入参数值均小于60
+    return Time(tt.hour, tt.minute, tt.second, dd);
+}
+
+// Time类成员函数
+// 计算当前时刻流逝了deltatime表示的时长后的新时刻
+Time Time::AddTime(Time deltatime)
+{
+    // 填写2：参照GetTimeInterval函数的处理思路，按提示补充代码（12分）
+    
+}
+
+int main()
+{
+    Time t0;                      // 创建时间对象t0: 默认0天0时0分0秒
+    Time t1(22, 30, 59);   // 创建时间对象t1，表示时刻: 22小时30分59秒
+    Time t2(75, 20);         // 创建时间对象t2，表示时刻: 75小时20分0秒
+
+    cout << "t0:" << t0.ToString() << endl;
+    cout << "t1:" << t1.ToString() << endl;
+    cout << "t2:" << t2.ToString() << endl;
+    cout << endl;
+
+    cout << "从t1时刻逝去30小时50分后的时刻为：" << endl;
+
+    // 填写3：计算从t1时刻逝去30时50分后的时刻，保存在时间对象t3中（4分）
+
+    cout << t3.ToString() << endl;
+    cout << endl;
+
+    cout << "从时刻t1到t2逝去的时间为：" << endl;
+    // 填写4：计算并输出从时刻t1到t2逝去的时间（时间间隔）（4分）
+
+}
+
+// 回答（10分，请在注解中直接作答）
+// 1) 说明Time类中返回值类型为int，以Get开头的4个公有成员函数的作用和特点。（4分）
+
+// 2) Time类的两个成员函数：GetTimeInterval和AddTime，输入参数均是同类对象，
+//     请你从减少对象拷贝次数的角度，考虑如何改进设计，优化程序性能？（6分）
